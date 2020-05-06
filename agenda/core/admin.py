5ca1d48ffb363fbc88657ae7d66b042eee61c4e3
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-06 11:59:08 -0300

    migrations

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Event
+
+class AdminEvent(admin.ModelAdmin):
+    list_display= ('title', 'event_date', 'creation_date')
+    list_filter = ('title', 'description', 'user')
 
 # Register your models here.
+admin.site.register(Event, AdminEvent)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(verbose_name="Título", max_length=100)
+    description= models.TextField(verbose_name="Descrição", blank=True, null=True)
+    event_date = models.DateTimeField(verbose_name="Data do Evento", auto_now=False, auto_now_add=False)
+    creation_date = models.DateTimeField(verbose_name="Data de Criação", auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
